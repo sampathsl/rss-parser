7d66984b2b862fc5de5f216dfb4a4d964a3ebe81
Sampath Thennakoon <sampath.thennakoon@gmail.com>
2021-10-20 14:59:19 +0530

    Add JavaDocs for DataInValidator

--- src/main/java/com/gifted/rss/util/DataInValidator.java
+++ src/main/java/com/gifted/rss/util/DataInValidator.java
@@ -7,11 +7,21 @@ import java.util.Arrays;
 public class DataInValidator implements ConstraintValidator<DataIn, String> {
     private String[] inputData;
 
+    /**
+     * Initiate the acceptable request parameter data
+     * @param data the {@link DataIn} object with acceptable values
+     */
     @Override
-    public void initialize(DataIn annotation) {
-        this.inputData = annotation.anyOf();
+    public void initialize(DataIn data) {
+        this.inputData = data.anyOf();
     }
 
+    /**
+     * Validate the incoming request parameter data
+     * @param value the incoming request parameter data
+     * @param context the {@link ConstraintValidatorContext} object
+     * @return the boolean value whether the incoming request parameter data valid or not
+     */
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         return Arrays.asList(inputData).contains(value);
